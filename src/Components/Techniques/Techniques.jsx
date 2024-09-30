7b2120c4dc568621d4dcabbbcf99070d4bdb4f28
justinait <justiiturraspe@gmail.com>
2024-09-30 19:26:22 -0300

    images

--- src/Components/Techniques/Techniques.jsx
+++ src/Components/Techniques/Techniques.jsx
@@ -6,9 +6,10 @@ function Techniques() {
     const techniques = [
         { name: 'VINILO', image: '/images/vinilo.png', text:'Aplicación y manipulación del vinilo a los vehículos, cambiando su color o diseño según la necesidad del cliente.', className: '' },
         { name: 'PPF', image: '/images/ppf.png', text:'Aplicación de la película de protección de pintura para proteger los vehículos contra daño y deterioro.', className: '' },
+        { name: 'LAVADO', image: '/images/wash.jpg', text:'Aplicación de la película de protección de pintura para proteger los vehículos contra daño y deterioro.', className: '' },
         { name: 'RESTAURACIÓN DE PINTURA', image: '/images/restauracionpintura.png', text:'Restauración y corrección de las imperfecciones en la pintura, así como devolverle al vehículo su lustre original.', className: '' },
-        { name: 'Pulida de llantas', image: '/images/pulidallantas.png', text:'Limpieza y pulida de llantas para restablecer su aspecto original y protegerlas del agua y las rocas.', className: '' },
-        { name: 'INTERIOR DETALLADO', image: '/images/interior.png', text:'Limpieza de paneles y detalles del interior del automóvil para mantenerlo en un estado inmaculado y limpio.', className: '' },
+        { name: 'PULIDA DE LLANTAS', image: '/images/pulidallantas.jpg', text:'Limpieza y pulida de llantas para restablecer su aspecto original y protegerlas del agua y las rocas.', className: '' },
+        { name: 'INTERIOR DETALLADO', image: '/images/interior.jpg', text:'Limpieza de paneles y detalles del interior del automóvil para mantenerlo en un estado inmaculado y limpio.', className: '' },
         { name: 'REVESTIMIENTO CERÁMICO', image: '/images/revestimiento.png', text:'Tratamiento que añade una capa protectora a la superficie del vehículo, bloqueado el daño del sol y los elementos.', className: '' }
     ]
     
@@ -78,8 +79,8 @@ function Techniques() {
 
             <div ref={containerRef} className='techniquesBoxsContainer'>
                 {
-                    techniques.map(e=> (
-                        <div className='techniqueBox'>
+                    techniques.map((e, i)=> (
+                        <div key={i} className='techniqueBox'>
                             <img src={e.image} alt="" />
                             <div className='techniqueInfoBox'>
                                 <h5>{e.name}</h5>
