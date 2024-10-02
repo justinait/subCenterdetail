f51f59ce43fc4cc7842606b000fc8b765df3ce8d
justinait <justiiturraspe@gmail.com>
2024-10-02 11:25:44 -0300

    hero

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import logo from '/logowhite.png'
 
 function Home() {
+  const [activeHero, setActiveHero] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveHero((prevHero) => (prevHero === 0 ? 1 : 0)); // Alterna entre los dos heroContainers
+    }, 4000); // Cambia cada 6 segundos
+
+    return () => clearInterval(interval); // Limpiar el intervalo al desmontar el componente
+  }, []);
   return (
     <div id='inicio'>
-      <div className='heroContainer firstHero'>
-        <h1>Visita el mejor <br /> <strong>servicio de detailing</strong> <br /> en Palma de Mallorca</h1>
+      <div className={`heroContainer ${activeHero === 0 ? 'firstHero' : ''}`}>
+        <h1>
+          {activeHero === 0 ? (
+            <>
+              Visita el mejor <br />
+              <strong>servicio de detailing</strong> <br />
+              en Palma de Mallorca
+            </>
+          ) : (
+            <>
+              Realza el brillo de tu auto con nuestro 
+              <strong> servicio de detailing </strong> 
+              en Palma de Mallorca
+            </>
+          )}
+        </h1>
         <img src={logo} alt="" className='heroLogo' />
       </div>
-      {/* <div className='heroContainer'>
+
+      {/* <div className={`heroContainer ${activeHero === 1 ? 'secondHero' : ''}`}>
         <h3>Realza el brillo de tu auto con nuestro <strong>servicio de detailing</strong> en Palma de Mallorca</h3>
         <img src={logo} alt="" className='heroLogo' />
       </div> */}
