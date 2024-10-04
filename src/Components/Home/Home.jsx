2722ceb8bf33ca87e77d739c5784a7694f4571f3
justinait <justiiturraspe@gmail.com>
2024-10-04 11:40:22 -0300

    hero desktop

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,13 +5,26 @@ import logo from '/logowhite.png'
 function Home() {
   const [activeHero, setActiveHero] = useState(0);
 
+  
   useEffect(() => {
-    const interval = setInterval(() => {
-      setActiveHero((prevHero) => (prevHero === 0 ? 1 : 0)); // Alterna entre los dos heroContainers
-    }, 4000); // Cambia cada 6 segundos
+    const updateHero = () => {
+      if (window.innerWidth < 1200) {
+        const interval = setInterval(() => {
+          setActiveHero((prevHero) => (prevHero === 0 ? 1 : 0)); // Alterna entre los dos heroContainers
+        }, 4000); // Cambia cada 6 segundos
 
-    return () => clearInterval(interval); // Limpiar el intervalo al desmontar el componente
+        return () => clearInterval(interval); // Limpiar el intervalo cuando se redimensiona o desmonta el componente
+      } else {
+        setActiveHero(1); // Si la pantalla es mayor a 1200px, fija el segundo contenido
+      }
+    };
+
+    updateHero(); // Llama a la función cuando se monta el componente
+    window.addEventListener('resize', updateHero); // Escucha cambios en el tamaño de la ventana
+
+    return () => window.removeEventListener('resize', updateHero); // Limpia el listener cuando se desmonta el componente
   }, []);
+
   return (
     <div id='inicio'>
       <div className={`heroContainer ${activeHero === 0 ? 'firstHero' : ''}`}>
