b05eb3ea79c1f175df5c0350b4d97ec4ec6f25c4
justinait <justiiturraspe@gmail.com>
2024-10-09 11:55:01 -0300

    wsp

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Contact.css'
 import ig from '/ig.png'
 import mail from '/mail.png'
+import wsp from '/wsp.png'
 
 function Contact() {
   return (
@@ -20,6 +21,10 @@ function Contact() {
         <div>
           <img src={mail} alt="CORREO" className='contactIconMail'/>
           <a href="mailto:centerdetailmallorca@gmail.com"  target="_blank" rel="noopener noreferrer" className='contactContainerP' style={{color: 'black', textDecoration: 'none'}}>centerdetailmallorca@gmail.com</a>
+        </div>
+        <div>
+          <img src={wsp} alt="WHATSAPP" className='contactIconMail'/>
+          <a className='contactContainerP' target='_blank' href='https://wa.link/rk36bo' rel="noopener noreferrer">+34 660 48 51 29</a>
         </div>
       </div>
 
